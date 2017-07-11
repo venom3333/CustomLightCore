44300779cf1674c60fdef82c9098f1d0ff385e51
Vladimir Venom <venom3333@gmail.com>
2017-07-11 15:59:07 +0300

    Проба KeyDerivation

--- CustomLightCore/Controllers/AccountController.cs
+++ CustomLightCore/Controllers/AccountController.cs
@@ -7,11 +7,35 @@ using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 using CustomLightCore.Models;
 using CustomLightCore.ViewModels;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
 
 namespace CustomLightCore.Controllers
 {
 	public class AccountController : BaseController
 	{
+
+		private string PasswordHash(string password)
+		{
+			string passwordHash = string.Empty;
+
+			// generate a 128-bit salt using a secure PRNG
+			byte[] salt = new byte[128 / 8];
+			using (var rng = RandomNumberGenerator.Create())
+			{
+				rng.GetBytes(salt);
+			}
+
+			passwordHash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+				password: password,
+				salt: salt,
+				prf: KeyDerivationPrf.HMACSHA1,
+				iterationCount: 10000,
+				numBytesRequested: 256 / 8));
+			return passwordHash;
+		}
+
 		[HttpGet]
 		public IActionResult Login()
 		{
@@ -24,7 +48,11 @@ namespace CustomLightCore.Controllers
 		{
 			if (ModelState.IsValid)
 			{
-				User user = await db.Users.FirstOrDefaultAsync(u => u.Login == model.Login && u.Password == model.Password);
+				/////////// Хэширование пароля //////////
+				string modelPasswordHash = PasswordHash(model.Password);
+				/////////////////////////////////////////
+
+				User user = await db.Users.FirstOrDefaultAsync(u => u.Login == model.Login && u.Password == modelPasswordHash);
 				if (user != null)
 				{
 					await Authenticate(model.Login);
@@ -52,7 +80,10 @@ namespace CustomLightCore.Controllers
 			if (ModelState.IsValid)
 			{
 				// добавляем пользователя в бд
-				db.Users.Add(new User { Login = model.Login, Password = model.Password });
+				/////////// Хэширование пароля //////////
+				string modelPasswordHash = PasswordHash(model.Password);
+				/////////////////////////////////////////
+				db.Users.Add(new User { Login = model.Login, Password = modelPasswordHash });
 				await db.SaveChangesAsync();
 
 				User user = await db.Users.FirstOrDefaultAsync(u => u.Login == model.Login);

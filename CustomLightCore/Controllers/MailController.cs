4db1b627e70bbde4fba6757092c4cf43f8015d38
Vladimir Venom <venom3333@gmail.com>
2017-08-20 23:27:30 +0300

    Функционал "CallBackMail"

---
+++ CustomLightCore/Controllers/MailController.cs
@@ -0,0 +1,127 @@
+
+
+namespace CustomLightCore.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using CustomLightCore.ViewModels.Mail;
+
+    using MailKit.Net.Smtp;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using MimeKit;
+
+    /// <summary>
+    /// The mail controller.
+    /// </summary>
+    public class MailController : BaseController
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MailController"/> class.
+        /// </summary>
+        public MailController()
+        {
+            this.FromAddress = "sanitarium667@gmail.com";
+            this.FromAdressTitle = "Перезвоните мне! (From Address)";
+            this.ToAddress = "sanitarium667@gmail.com";
+            this.ToAdressTitle = "Перезвоните мне! (To Address)";
+        }
+
+        /// <summary>
+        /// Gets or sets the from address.
+        /// </summary>
+        private string FromAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the from adress title.
+        /// </summary>
+        private string FromAdressTitle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the to address.
+        /// </summary>
+        private string ToAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the to adress title.
+        /// </summary>
+        private string ToAdressTitle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subject.
+        /// </summary>
+        private string Subject { get; set; }
+
+        /// <summary>
+        /// Gets or sets the body content.
+        /// </summary>
+        private string BodyContent { get; set; }
+
+        /// <summary>
+        /// The call back mail.
+        /// </summary>
+        /// <param name="callBackForm">
+        /// The call Back Form.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IActionResult"/>.
+        /// </returns>
+        /// TODO: Переделать на AJAX
+        [HttpPost]
+        public IActionResult CallBackMail(CallBackMailViewModel callBackForm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    this.Subject = "Просьба перезвонить!";
+
+                    this.BodyContent = string.Format(
+                        @"
+                    Просьба перезвонить!
+                    Имя: {0}
+                    Телефон: {1}
+                    Примечания: {2}",
+                        callBackForm.Name,
+                        callBackForm.Phone,
+                        callBackForm.Misc);
+
+                    // Smtp Server 
+                    const string SmtpServer = "smtp.gmail.com";
+
+                    // Smtp Port Number 
+                    const int SmtpPortNumber = 587;
+
+                    var mimeMessage = new MimeMessage();
+                    mimeMessage.From.Add(new MailboxAddress(FromAdressTitle, FromAddress));
+                    mimeMessage.To.Add(new MailboxAddress(ToAdressTitle, ToAddress));
+                    mimeMessage.Subject = Subject;
+                    mimeMessage.Body = new TextPart("plain") { Text = BodyContent };
+
+                    using (var client = new SmtpClient())
+                    {
+
+                        client.Connect(SmtpServer, SmtpPortNumber, false);
+
+                        // Note: only needed if the SMTP server requires authentication 
+                        // Error 5.5.1 Authentication  
+                        client.Authenticate(FromAddress, "aeseJ1zZ");
+                        client.Send(mimeMessage);
+                        client.Disconnect(true);
+                    }
+
+                    return RedirectToAction("Index", "Categories");
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return this.PartialView("_CallBackMail");
+        }
+    }
+}
---
+++ CustomLightCore/ViewModels/Mail/CallBackMailViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace CustomLightCore.ViewModels.Mail
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The call back mail view model.
+    /// </summary>
+    public class CallBackMailViewModel
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        [Required]
+        [DisplayName("Имя")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone.
+        /// </summary>
+        [Required]
+        [DisplayName("Телефон")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the misc.
+        /// </summary>
+        [DisplayName("Примечания")]
+        [DataType(DataType.MultilineText)]
+        public string Misc { get; set; }
+    }
+}
